e7352e17be870e47ca668b1e7cbdc2a520435447
onlyhappiness <onlyhappiness96@gmail.com>
2022-03-27 23:18:25 +0900

    [UPDATE] 내가 직접 만든다.

---
+++ blog/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+// import './App.css';
+import Header from './header';
+import Posts from './posts';
+
+function App() {
+  return (
+    <div className='App py-6'>
+      <Header />
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ blog/src/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header '>
+      Header
+      <style jsx>{`
+        .header {
+          border: 1px solid black;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Header;
